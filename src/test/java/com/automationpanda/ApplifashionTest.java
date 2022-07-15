cd329f2767b32e022da001b5292422e806978b93
Pandy Knight <andylpk.247@gmail.com>
2022-07-15 17:09:59 -0400

    Rewrote capabilities to use environment variables

--- src/test/java/com/automationpanda/ApplifashionTest.java
+++ src/test/java/com/automationpanda/ApplifashionTest.java
@@ -2,6 +2,7 @@ package com.automationpanda;
 
 import io.appium.java_client.AppiumDriver;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -13,25 +14,57 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplifashionTest {
+
+    private static String appiumUrl;
+    private static DesiredCapabilities capabilities;
 
     private AppiumDriver driver;
     private WebDriverWait wait;
 
-    @BeforeEach
-    public void initDriver() throws IOException {
+    private static DesiredCapabilities buildCapabilities() {
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        // Read and build app path
+        String appVersion = System.getenv().getOrDefault("APP_BRANCH", "main");
+        assertTrue(Arrays.asList("main", "dev", "prod").contains(appVersion));
+        String appPath = "/Users/automationpanda/Desktop/Applifashion/" + appVersion + "-app-debug.apk";
+
+        // Read inputs from environment variables
+        String platformVersion = System.getenv().getOrDefault("APPIUM_PLATFORM_VERSION", "12");
+        String deviceName = System.getenv().getOrDefault("APPIUM_DEVICE_NAME", "Pixel 3a API 32");
+
+        // Create capabilities
+        capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "android");
         capabilities.setCapability("appium:automationName", "uiautomator2");
-        capabilities.setCapability("appium:platformVersion", "12");
-        capabilities.setCapability("appium:deviceName", "Pixel 3a API 32");
-        capabilities.setCapability("appium:app", "/Users/automationpanda/Desktop/Applifashion/main-app-debug.apk");
+        capabilities.setCapability("appium:platformVersion", platformVersion);
+        capabilities.setCapability("appium:deviceName", deviceName);
+        capabilities.setCapability("appium:app", appPath);
         capabilities.setCapability("appium:appPackage", "com.applitools.applifashion.main");
         capabilities.setCapability("appium:appActivity", "com.applitools.applifashion.main.activities.MainActivity");
 
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        // Return the capabilities
+        return capabilities;
+    }
+
+    @BeforeAll
+    public static void setUpAllTests() {
+
+        // Read the Appium URL
+        appiumUrl = System.getenv().getOrDefault("APPIUM_URL", "http://127.0.0.1:4723/wd/hub");
+
+        // Create the capabilities
+        capabilities = buildCapabilities();
+    }
+
+    @BeforeEach
+    public void initDriver() throws IOException {
+
+        driver = new AppiumDriver(new URL(appiumUrl), capabilities);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
