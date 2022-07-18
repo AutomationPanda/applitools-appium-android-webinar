4677672b17bea81f074d91e6a77a8e9325d90b2d
Pandy Knight <andylpk.247@gmail.com>
2022-07-18 11:19:16 -0400

    Added instrumented apk files to test resources

--- src/test/java/com/automationpanda/ApplifashionTest.java
+++ src/test/java/com/automationpanda/ApplifashionTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
@@ -30,35 +31,42 @@ public class ApplifashionTest {
         return System.getenv().getOrDefault("APPIUM_URL", "http://127.0.0.1:4723/wd/hub");
     }
 
-    private static DesiredCapabilities buildCapabilities() {
+    private static String readPlatformVersion() {
+        return System.getenv().getOrDefault("APPIUM_PLATFORM_VERSION", "12");
+    }
+
+    private static String readDeviceName() {
+        return System.getenv().getOrDefault("APPIUM_DEVICE_NAME", "Pixel 3a API 32");
+    }
+
+    private static String readAppPath() {
 
         // Read the app branch
         String appVersion = System.getenv().getOrDefault("APP_BRANCH", "main");
         assertTrue(Arrays.asList("main", "dev", "prod").contains(appVersion));
 
-        // Read or set capability values
-        // Hard-coding these values is typically not a recommended practice
-        // Instead, you should read them from a resource file (like a properties or JSON file)
-        // They are set here like this to make this example code simpler
-        final String platformName = "android";
-        final String automationName = "uiautomator2";
-        final String platformVersion = System.getenv().getOrDefault("APPIUM_PLATFORM_VERSION", "12");
-        final String deviceName = System.getenv().getOrDefault("APPIUM_DEVICE_NAME", "Pixel 3a API 32");
-        final String appPath = "/Users/automationpanda/Desktop/Applifashion/" + appVersion + "-app-debug.apk";
-        final String appPackage = "com.applitools.applifashion.main";
-        final String appActivity = "com.applitools.applifashion.main.activities.MainActivity";
-        final String fullReset = "true";
+        // Get the apk folder path from the resources directory
+        String apkFolderPath = ApplifashionTest.class.getClassLoader().getResource("instrumented-apk").getPath();
+
+        // Concatenate and return the app path
+        return apkFolderPath + File.separator + appVersion + "-app-inst.apk";
+    }
+
+    private static DesiredCapabilities buildCapabilities() {
 
         // Create capabilities
+        // Hard-coding these values is typically not a recommended practice
+        // Instead, they should be read from a resource file (like a properties or JSON file)
+        // They are set here like this to make this example code simpler
         capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", platformName);
-        capabilities.setCapability("appium:automationName", automationName);
-        capabilities.setCapability("appium:platformVersion", platformVersion);
-        capabilities.setCapability("appium:deviceName", deviceName);
-        capabilities.setCapability("appium:app", appPath);
-        capabilities.setCapability("appium:appPackage", appPackage);
-        capabilities.setCapability("appium:appActivity", appActivity);
-        capabilities.setCapability("appium:fullReset", fullReset);
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("appium:automationName", "uiautomator2");
+        capabilities.setCapability("appium:platformVersion", readPlatformVersion());
+        capabilities.setCapability("appium:deviceName", readDeviceName());
+        capabilities.setCapability("appium:app", readAppPath());
+        capabilities.setCapability("appium:appPackage", "com.applitools.applifashion.main");
+        capabilities.setCapability("appium:appActivity", "com.applitools.applifashion.main.activities.MainActivity");
+        capabilities.setCapability("appium:fullReset", "true");
 
         // Return the capabilities
         return capabilities;
